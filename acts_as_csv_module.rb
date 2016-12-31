969b69245c5a6ff1af984a6bee34b37a42f8d32d
cgeb <ckg61386@gmail.com>
2016-12-31 12:24:05 +0900

    create base module

---
+++ acts_as_csv_module.rb
@@ -0,0 +1,58 @@
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def initialize
+      read
+    end
+
+    def each
+      @csv_contents.each do |row|
+        yield CsvRow.new(@headers, row)
+      end
+    end
+    
+  end
+
+  class CsvRow
+    def initialize(headers, row)
+      @headers = headers
+      @row = row
+    end
+    
+    def method_missing(name)
+      method_index = @headers.index(name.to_s)
+      @row[method_index] if method_index
+    end
+  end
+  
+end
+
+class RubyCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+m.each { |row| puts row.one }
